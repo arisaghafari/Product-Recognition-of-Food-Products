557fcac46f4ecf0d7cafb559d9f45b9231387f04
arisaghafari <arisaghafari68@gmail.com>
2024-07-02 19:42:39 +0200

    fix errors

---
+++ Single_Instance_Detection/Single_Product_Recognition.py
@@ -0,0 +1,93 @@
+# from google.colab import drive
+# drive.mount('/content/drive')
+
+# # do this just for the firt time
+# !unzip /content/drive/MyDrive/VisionProject/dataset.zip -d /content/drive/MyDrive/VisionProject/images/
+
+import cv2
+import numpy as np
+# from google.colab.patches import cv2_imshow
+from matplotlib import pyplot as plt
+
+def denoising(noisy_img):
+
+  median_denoised = cv2.medianBlur(noisy_img, 5)
+  # nl_means_denoised = cv2.fastNlMeansDenoisingColored(median_denoised, None, 5, 5, 5, 21)
+  bilateral_denoised = cv2.bilateralFilter(median_denoised, d=7, sigmaColor=75, sigmaSpace=75)
+
+  return bilateral_denoised
+
+def match(des_query, des_train):
+  # Defining index for approximate kdtree algorithm
+  FLANN_INDEX_KDTREE = 1
+
+  # Defining parameters for algorithm
+  index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+
+  # Defining search params.
+  # checks=50 specifies the number of times the trees in the index should be recursively traversed.
+  # Higher values gives better precision, but also takes more time
+  search_params = dict(checks = 50)
+
+  # Initializing matcher
+  flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+  # Matching and finding the 2 closest elements for each query descriptor.
+  matches = flann.knnMatch(des_query,des_train,k=2)
+
+  good_matches = []
+  threshold_distance = 0.65
+  for m,n in matches:
+    if m.distance < threshold_distance*n.distance:
+      good_matches.append(m)
+
+  return good_matches
+
+def plot_images(images):
+
+    fig = plt.figure(figsize=(30 ,30))
+    rows = 1
+    columns = 1
+
+    if len(images) % 2 == 0:
+        columns = int(len(images) / 2)
+        rows = int(len(images) / 2)
+    else:
+        columns = int(len(images) / 2) + 1
+        rows = int(len(images) / 2) +1
+
+
+    for i in range(len(images)):
+        fig.add_subplot(rows, columns, i+1)
+        plt.imshow(images[i])
+        plt.axis('off')
+
+    plt.show()
+    # f, axs = plt.subplots(rows, columns, figsize=(30,30))
+    # count = 0
+    # for x in axs.flatten(): 
+    #     if count != len(images):
+    #         x.imshow(images[count]) ; x.axis('off')
+    #         count += 1
+    # plt.show()
+
+
+
+def template_matching_Zncc(image, template):
+    
+    h_i, w_i = image.shape
+  
+    x, y, w_t, h_t = cv2.boundingRect(template)
+
+    if h_i < h_t:
+        template = template[y:y+h_i, x:x+w_t]
+
+    if w_i < w_t:
+        template = template[y:y+h_t, x:x+w_i]
+
+    result = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+
+    # Find the location of the best match
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+    return max_val
---
+++ Single_Instance_Detection/main.py
@@ -0,0 +1,92 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from Single_Product_Recognition import match, denoising, plot_images, template_matching_Zncc
+import random
+
+def main():
+    MIN_MATCH_COUNT = 20
+    MIN_MATCH_MASK_COUNT = 10
+    MIN_MAX_VAL = 0.37
+    
+    # Initiate SIFT detector
+    sift = cv2.xfeatures2d.SIFT_create()
+
+    # Load reference images
+    reference_images = []
+    reference_keypoints = []
+    reference_descriptors = []
+
+    for i in range(1,15):
+    #   img = cv2.imread(f'drive/MyDrive/VisionProject/images/dataset/models/ref{i}.png', cv2.IMREAD_GRAYSCALE)
+        img = cv2.imread(f'../dataset/models/ref{i}.png', cv2.IMREAD_GRAYSCALE)
+
+        reference_images.append(img)
+        kp, des = sift.detectAndCompute(img, None)
+        reference_keypoints.append(kp)
+        reference_descriptors.append(des)
+
+    images = []
+    for i in range(1, 6):
+    #   img_train = cv2.imread(f'drive/MyDrive/VisionProject/images/dataset/scenes/scene{i}.png')
+        img_train = cv2.imread(f'../dataset/scenes/scene{i}.png')
+        img_train_d = cv2.cvtColor(denoising(img_train), cv2.COLOR_BGR2GRAY)
+
+        kp_train = sift.detect(img_train_d)
+        kp_train, des_train = sift.compute(img_train_d, kp_train)
+
+        for k in range(len(reference_images)):
+            
+            good_matches = match(reference_descriptors[k], des_train)
+            lgm = len(good_matches)
+
+            if lgm >= MIN_MATCH_COUNT:
+                # print(f"scene{i} in ref{k+1} - number of matches: {lgm}")
+
+                # building the corrspondences arrays of good matches
+                src_pts = np.float32([ reference_keypoints[k][m.queryIdx].pt for m in good_matches ]).reshape(-1,1,2)
+                dst_pts = np.float32([ kp_train[m.trainIdx].pt for m in good_matches ]).reshape(-1,1,2)
+
+                # Using RANSAC to estimate a robust homography.
+                # It returns the homography M and a mask for the discarded points
+                M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 4.0)
+
+                # Mask of discarded point used in visualization
+                matchesMask = mask.ravel().tolist()
+                l = sum(matchesMask)
+
+                if l >= MIN_MATCH_MASK_COUNT:
+
+                    # Corners of the query image
+                    h,w = reference_images[k].shape
+                    
+                    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+                    
+                    # Projecting the corners into the train image
+                    dst = cv2.perspectiveTransform(pts,M)
+                    
+                    # Changing the size of template base on target that the algorithm found
+                    dst_int = np.int32(dst)
+                    x, y, w_d, h_d = cv2.boundingRect(dst_int)
+                    dsize = (w_d, h_d)
+                
+                    # Resize the image
+                    resized_image = cv2.resize(reference_images[k], dsize, interpolation=cv2.INTER_LINEAR)
+                    
+                    # Template_matching
+                    max_Val = template_matching_Zncc(img_train_d, resized_image)
+                    
+                    if max_Val >= MIN_MAX_VAL:
+                        r = random.randint(0, 1)
+                        b = random.randint(0, 1)
+                        if r == 1 and b == 1:
+                            b = 0
+                        # Drawing the bounding box
+                        cv2.polylines(img_train,[np.int32(dst)],True,(r*255, 255, b*255),15, cv2.LINE_AA)
+
+        images.append(cv2.cvtColor(img_train, cv2.COLOR_BGR2RGB)) 
+     
+    plot_images(images)
+
+if __name__ == "__main__":
+    main()
