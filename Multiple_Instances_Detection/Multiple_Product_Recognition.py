f310e60582d2bbc7be0fc53c0131fda94729f8ba
arisaghafari <arisaghafari68@gmail.com>
2024-07-12 16:08:22 +0200

    modify code structure

--- Multiple_Instances_Detection/Multiple_Product_Recognition.py
+++ Multiple_Instances_Detection/Multiple_Product_Recognition.py
@@ -6,10 +6,18 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from Single_Instance_Detection.Single_Product_Recognition import *
 import math
+from HoughTransform import *
 
-def find_matching_boxes(image, refrence_images, params):
+IMAGE_INDEX = 0
+KEY_INDEX = 1
+DES_INDEX = 2
+CENTER_INDEX = 3
+V_INDEX = 4
+
+def find_matching_boxes(img_train, refrence_images_features, params):
+    
     MAX_RATIO = 0.55
-    MAX_DISTANCE = 14
+    MAX_DISTANCE = 44
     MIN_MATCH_COUNT = 260
     MIN_MATCH_MASK_COUNT = 10
     # Parameters and their default values
@@ -21,27 +29,39 @@ def find_matching_boxes(image, refrence_images, params):
     bf = cv2.BFMatcher()
 
     matched_boxes = []
-    for i in range(len(refrence_images)):
-        matching_img = image.copy()
-        # Find keypoints and descriptors for the template
-        keypoints2, descriptors2 = detector.detectAndCompute(refrence_images[i], None)
+    for key, value in refrence_images_features.items():
+        matching_img = img_train.copy()
 
         flag = True
         count = 0
         while(flag):
             # Match descriptors
-            keypoints1, descriptors1 = detector.detectAndCompute(matching_img, None)
+            keypoints_s, descriptors_s = detector.detectAndCompute(matching_img, None)
+
             # Matching strategy for SIFT
-            matches = bf.knnMatch(descriptors1, descriptors2, k=2)
+            matches = bf.knnMatch(descriptors_s, value[DES_INDEX], k=2)
             good_matches = [m for m, n in matches if m.distance < SIFT_DISTANCE_THRESHOLD * n.distance]
             good_matches = sorted(good_matches, key=lambda x: x.distance)[:BEST_MATCHES_POINTS]
             
+
             if len(good_matches) >= MIN_MATCH_COUNT:
                 print("there is enough match numbers : ", len(good_matches))    
-                print(f"there is {count + 1} * template_{i + 15}")
                 # Extract location of good matches
-                points1 = np.float32([keypoints1[m.queryIdx].pt for m in good_matches])
-                points2 = np.float32([keypoints2[m.trainIdx].pt for m in good_matches])
+
+                #Hough 
+                # V = VotingVectors(good_matches,refrence_images_features, key)
+                # ACC, G_scene, Accumulator_Array_Points = Accumulator_Array(ACC_ARRAY_CELL_DIMENSION_1,
+                #                                                       ACC_ARRAY_CELL_DIMENSION_2,
+                #                                                       scene_img, 
+                #                                                       model_img,
+                #                                                       model_images_features, 
+                #                                                       m_scenes_images_features, 
+                #                                                       good, 
+                #                                                       V, 
+                #                                                       i, j)
+
+                points1 = np.float32([keypoints_s[m.queryIdx].pt for m in good_matches])
+                points2 = np.float32([value[KEY_INDEX][m.trainIdx].pt for m in good_matches])
 
                 # Find homography for drawing the bounding box
 
@@ -50,28 +70,35 @@ def find_matching_boxes(image, refrence_images, params):
                 matchesMask = mask.ravel().tolist()
                 l = sum(matchesMask)
 
-                dis_sum, counter = color_matching(keypoints1, keypoints2, matchesMask, matching_img, refrence_images[i], good_matches)
+                dis_sum, counter = color_matching(keypoints_s,
+                                                   value[KEY_INDEX], matchesMask, matching_img, value[IMAGE_INDEX], good_matches)
                 ratio = counter / l
 
                 print("distance : ", dis_sum)
                 print("ratio : ", ratio)
                 
-                print("match mastk : ", l)
-                if l >= MIN_MATCH_MASK_COUNT and (ratio <= MAX_RATIO or dis_sum <= MAX_DISTANCE):
+                print(f"template_{key} --> match mastk : {l}")
+                if l >= MIN_MATCH_MASK_COUNT: #or dis_sum <= MAX_DISTANCE):
+                    
+                    print("pass the threshold ....")
+                    print(f"there is {count + 1} * template_{key}")
                     # template_matching(points2)
                     # Transform the corners of the template to the matching points in the image
-                    h, w = refrence_images[i].shape[:2]
+                    h, w = value[IMAGE_INDEX].shape[:2]
                     corners = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
                     transformed_corners = cv2.perspectiveTransform(corners, H)
-                    matched_boxes.append((transformed_corners, i))
+                    if ratio <= MAX_RATIO and dis_sum <= MAX_DISTANCE:
+                        matched_boxes.append((transformed_corners, key))
+                        print("injam!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                        # You can uncomment the following lines to see the matching process
+                        # Draw the bounding box
+                        img1_with_box = matching_img.copy()
+                        matching_result = cv2.drawMatches(img1_with_box, keypoints_s, value[IMAGE_INDEX], value[KEY_INDEX], good_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+                        cv2.polylines(matching_result, [np.int32(transformed_corners)], True, (255, 0, 0), 3, cv2.LINE_AA)
+                        plt.imshow(matching_result, cmap='gray')
+                        plt.show()
 
-                    # # You can uncomment the following lines to see the matching process
-                    # # Draw the bounding box
-                    # img1_with_box = matching_img.copy()
-                    # matching_result = cv2.drawMatches(img1_with_box, keypoints1, refrence_images[i], keypoints2, good_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-                    # cv2.polylines(matching_result, [np.int32(transformed_corners)], True, (255, 0, 0), 3, cv2.LINE_AA)
-                    # plt.imshow(matching_result, cmap='gray')
-                    # plt.show()
+                        count += 1
 
                     # Create a mask and fill the matched area with near neighbors
                     matching_img2 = cv2.cvtColor(matching_img, cv2.COLOR_BGR2GRAY) 
@@ -79,16 +106,15 @@ def find_matching_boxes(image, refrence_images, params):
                     cv2.fillPoly(mask, [np.int32(transformed_corners)], 0)
                     mask = cv2.bitwise_not(mask)
                     matching_img = cv2.inpaint(matching_img, mask, 3, cv2.INPAINT_TELEA)
-
-                    count += 1
+                    
                 else:
                     print("there isn't enough match MASK numbers : ", l)
-                    print(f"template_{i + 15} is not there..")
+                    print(f"template_{key} is not there..")
                     flag = False
             
             else:
                 print("there isn't enough match numbers : ", len(good_matches))
-                print(f"template_{i + 15} is not there..")
+                print(f"template_{key} is not there..")
                 flag = False
 
     return matched_boxes
@@ -99,7 +125,7 @@ def plot_boxes(img1, matched_boxes):
         cv2.polylines(img1, [np.int32(box[0])], True, (0, 255, 0), 3, cv2.LINE_AA)
         # print("box ...................... : ",box)
         x, y, w_d, h_d = cv2.boundingRect(box[0])
-        cv2.putText(img1, f"refrence_{box[1] + 15}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+        cv2.putText(img1, f"refrence_{box[1]}", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
     plt.imshow(img1)
     plt.show()
 
@@ -110,13 +136,21 @@ def denoise(src):
     am_filter = cv2.ximgproc.createAMFilter(sigma_s, sigma_r, False)
 
     # Apply the filter
-    dst = denoising(src)
+    dst = denoising1(src)
     dst = am_filter.filter(dst)
 
     return dst
 
+def denoising1(noisy_img):
+
+  median_denoised = cv2.medianBlur(noisy_img, 5)
+  nl_means_denoised = cv2.fastNlMeansDenoisingColored(median_denoised, None, 5, 5, 5, 21)
+  bilateral_denoised = cv2.bilateralFilter(nl_means_denoised, d=7, sigmaColor=75, sigmaSpace=75)
+
+  return bilateral_denoised
+
 def color_matching(key_train, key_ref, matchmask, img_train, img_ref, good_matches):
-    MAX_DIST = 60
+    MAX_DIST = 34
     print("................ATTENTION........................")
     img_t = np.copy(img_train)
     img_r = np.copy(img_ref)
@@ -133,12 +167,13 @@ def color_matching(key_train, key_ref, matchmask, img_train, img_ref, good_match
 
     sum = 0
     counter = 0
+    dis_counter = 0
     for i in range(len(matchmask)):
         if matchmask[i] == 1: #inlier
             x_ref = int(point_ref[i][0])
             y_ref = int(point_ref[i][1])
             size_r = int(size_ref[i] * math.sqrt(2))
-            size_r = 100
+            # size_r = 100
             window_red_r = img_r[y_ref:y_ref+size_r, x_ref:x_ref+size_r, 0]
             window_green_r = img_r[y_ref:y_ref+size_r, x_ref:x_ref+size_r, 1]
             window_blue_r = img_r[y_ref:y_ref+size_r, x_ref:x_ref+size_r, 2]
@@ -167,15 +202,16 @@ def color_matching(key_train, key_ref, matchmask, img_train, img_ref, good_match
                         return 1000000, 100000  
 
                     dist = color_distance_compute(value_1, value_2)
-
+                    sum += dist
+                    dis_counter += 1
                     print("distance : ", dist)
                     if dist >= MAX_DIST:
                         counter += 1
                 
-                    sum += dist
+                    
 
     print(counter)
-    return (sum/len(matchmask), counter)
+    return (sum/dis_counter, counter)
             
 
     
--- Multiple_Instances_Detection/main.py
+++ Multiple_Instances_Detection/main.py
@@ -7,18 +7,35 @@ from Multiple_Product_Recognition import *
 
 
 def main():
-    refrence_images = []
+    mean_of_model_intensities_r_g_b = {}
+    refrence_images_features = {}
+
+    detector = cv2.SIFT_create()
+
     for i in range(15, 28):
         template = cv2.imread(f'../dataset/models/ref{i}.png') # Template
         template = cv2.cvtColor(template, cv2.COLOR_BGR2RGB)
-        refrence_images.append(template)
+
+        # Find keypoints and descriptors for the template
+        keypoints_r, descriptors_r = detector.detectAndCompute(template, None)
+
+        refrence_images_features[i] = [template, keypoints_r, descriptors_r]
+        #Find or update the center of the model image information collected in the model features dictionary
+        InstanceCenter(template,refrence_images_features, i) 
+
+        # refrence_images.append(template)
+        
+        b,g,r = cv2.split(template)
+
+        # save the mean of the intensities (divided per channel) for every model image
+        mean_of_model_intensities_r_g_b[i] = [np.mean(r), np.mean(g), np.mean(b)]
 
     params = {
         'SIFT_distance_threshold': 0.85,
         'best_matches_points': 500
     }
 
-    for i in range(8, 9):
+    for i in range(6, 7):
         print(f".......................SCENE{i}......................\n")
         img1 = cv2.imread(f'../dataset/scenes/scene{i}.png') # Image
         img_copy = img1.copy()
@@ -29,7 +46,7 @@ def main():
         img1_d = cv2.cvtColor(img1_d, cv2.COLOR_BGR2RGB)
 
         # Change to "SIFT" or "ORB" depending on your requirement
-        matched_boxes = find_matching_boxes(img1_d, refrence_images, params) 
+        matched_boxes = find_matching_boxes(img1_d, refrence_images_features, params) 
 
         # Draw the bounding boxes on the original image
         plot_boxes(img1, matched_boxes)    
